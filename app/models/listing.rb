b596ab79d98a97adf3c20cbbac80e75eba4edca9
NathanielMether <nmet3@student.monash.edu>
2017-10-11 12:49:02 +1100

    full adress method

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,8 @@ class Listing < ApplicationRecord
   def country
     ISO3166::Country.new(country_code.upcase)
   end
+
+  def full_address
+    "#{street_address}, #{city}, #{country_name}"
+  end
 end
