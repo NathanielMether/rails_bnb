f6aaa9c939011328ff9ee9260cf34540984760f0
NathanielMether <nmet3@student.monash.edu>
2017-10-11 13:01:54 +1100

    add geocode

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,12 @@
 class Listing < ApplicationRecord
+  geocoded_by :full_address
+  after_validation :geocode
+
   def country
     ISO3166::Country.new(country_code.upcase)
   end
 
   def full_address
-    "#{street_address}, #{city}, #{country_name}"
+    "#{street_address}, #{city}, #{country}"
   end
 end
